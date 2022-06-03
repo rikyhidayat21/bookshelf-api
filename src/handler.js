b835bbe3af2348f50e5e78e08f457c218c7c3632
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-06-03 14:04:53 +0700

    read book by id

--- src/handler.js
+++ src/handler.js
@@ -81,7 +81,32 @@ const getAllBooksHandler = () => ({
   },
 });
 
+const getBookByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const book = books.filter((b) => b.id === id)[0];
+
+  if (book !== undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
+  getBookByIdHandler,
 };
